cb5f59786a7daba7a531e845c1110170550282cc
frankalvarezdev <frankcontacto64@gmail.com>
2021-11-30 18:34:25 -0500

    add new route

--- app/app.php
+++ app/app.php
@@ -6,8 +6,19 @@ const TEMPLATE = 'views:template.php';
 $app = new App();
 $app->path('views', __DIR__ . '/views');
 
-$app->bind("/", function () {
-    return $this->render(TEMPLATE);
+// import routes
+require_once __DIR__ . '/config/routes.php';
+
+// render custom error pages
+$app->on("after", function() {
+    switch($this->response->status){
+        case "404":
+            $this->response->body = $this->render(TEMPLATE);
+            break;
+        case "500":
+            $this->response->body = 'ERROR 500';
+            break;
+    }
 });
 
 $app->run();
--- app/config/routes.php
+++ app/config/routes.php
@@ -1,2 +1,9 @@
 <?php
 
+$app->get("/", function () {
+    return $this->render(TEMPLATE);
+});
+
+$app->get("/hello/:name", function () {
+    return $this->render(TEMPLATE);
+});
