dbc59f0ae8663de261ca643cd91a4fd9242cc576
frankalvarezdev <frankcontacto64@gmail.com>
2021-11-30 18:18:58 -0500

    add script source

--- app/views/template.php
+++ app/views/template.php
@@ -5,8 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="/build/bundle.js" defer></script>
 </head>
 <body>
-    <h1>Hola mundo</h1>
+    <main-app></main-app>
 </body>
 </html>
